cf748e67ac6e83943f6925f339b63f2048b6e845
yoshidacojp <yoshidacojp@gmail.com>
2020-01-01 14:21:05 +0900

    個別ページテンプレートを追加

---
+++ html/wp-content/themes/easiest-wp/page.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<?php while (have_posts()) { ?>
+	<?php the_post(); ?>
+
+	<div class="page-title">
+		<h1><?php the_title(); ?></h1>
+	</div>
+
+	<div class="content-area content-area-profile">
+		<div class="main-column">
+			<div class="box-content radius-tl">
+				<article><?php the_content(); ?></article>
+			</div>
+		</div>
+
+		<?php if (comments_open() || get_comments_number()) {
+			comments_template();
+		} ?>
+	</div>
+
+<?php } ?>
+
+<?php get_footer();
