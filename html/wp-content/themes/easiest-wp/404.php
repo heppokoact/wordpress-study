45785d62def2c5464475ac4dd2d4079342fc5486
yoshidacojp <yoshidacojp@gmail.com>
2020-01-01 14:55:44 +0900

    404ページを追加

---
+++ html/wp-content/themes/easiest-wp/404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<div class="page-title">
+	<h1>404 Not Found</h1>
+</div>
+<div class="content-area has-side-col">
+	<div class="main-column">
+		<h1 class="box-heading box-heading-main-col">404 Not Found</h1>
+		<div class="box-content">
+			<p>お探しのページは見つかりませんでした。</p>
+		</div>
+
+	</div>
+
+	<?php get_sidebar(); ?>
+
+</div>
+
+<?php get_footer(); ?>
